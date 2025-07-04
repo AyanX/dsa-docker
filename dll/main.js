732ce145715bcf533109384b2b4d0794dba6cd0d
AyanX <fearthesorcerer@gmail.com>
2025-07-04 19:48:39 +0300

    dll

--- dll/main.js
+++ dll/main.js
@@ -26,10 +26,45 @@ class Dl{
         this.tail=newNode
         
     }
+    pop(){
+        if(!this.head){
+            return 0;
+        }
+
+        if(!this.head.next){
+            this.head=null
+            this.tail=null
+            return 1
+        }
+
+
+        let temp = this.tail
+        this.tail = this.tail.prev
+        this.tail.next = null
+        temp.prev = null
+
+        return 1
+    }
+    unshift(value){ 
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head=newNode
+            this.tail=this.head
+            return 1;
+        }
+        this.head.prev = newNode
+        newNode.next = this.head
+
+        this.head=newNode
+        return 1
+    }
 
 }
 
 const dl = new Dl(10)
 dl.push(7)
 dl.push(9)
-log(dl)
+dl.pop()
+dl.push(11)
+dl.unshift(2)
+log(dl.head)
